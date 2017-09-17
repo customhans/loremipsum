fe96e13190077273764ac0bfbc5b1570218406ec
Hannes Flaschentraeger <hannesflaschentraeger@gmail.com>
2017-09-17 16:30:37 +0200

    V1 - LI generator works including specified number of words.

---
+++ main.js
@@ -0,0 +1,46 @@
+var vocab = [
+		'lorem', 'ipsum', 'dolor', 'sit', 'amet', 'consectetur',
+		'adipiscing', 'elit', 'curabitur', 'vel', 'hendrerit', 'libero',
+		'eleifend', 'blandit', 'nunc', 'ornare', 'odio', 'ut',
+		'orci', 'gravida', 'imperdiet', 'nullam', 'purus', 'lacinia',
+		'a', 'pretium', 'quis', 'congue', 'praesent', 'sagittis',
+		'laoreet', 'auctor', 'mauris', 'non', 'velit', 'eros',
+		'dictum', 'proin', 'accumsan', 'sapien', 'nec', 'massa',
+		'volutpat', 'venenatis', 'sed', 'eu', 'molestie', 'lacus',
+		'quisque', 'porttitor', 'ligula', 'dui', 'mollis', 'tempus',
+		'at', 'magna', 'vestibulum', 'turpis', 'ac', 'diam',
+		'tincidunt', 'id', 'condimentum', 'enim', 'sodales', 'in',
+		'hac', 'habitasse', 'platea', 'dictumst', 'aenean', 'neque',
+		'fusce', 'augue', 'leo', 'eget', 'semper', 'mattis',
+		'tortor', 'scelerisque', 'nulla', 'interdum', 'tellus', 'malesuada',
+		'rhoncus', 'porta', 'sem', 'aliquet', 'et', 'nam',
+		'suspendisse', 'potenti', 'vivamus', 'luctus', 'fringilla', 'erat',
+		'donec', 'justo', 'vehicula', 'ultricies', 'varius', 'ante',
+		'primis', 'faucibus', 'ultrices', 'posuere', 'cubilia', 'curae',
+		'etiam', 'cursus', 'aliquam', 'quam', 'dapibus', 'nisl',
+		'feugiat', 'egestas', 'class', 'aptent', 'taciti', 'sociosqu',
+		'ad', 'litora', 'torquent', 'per', 'conubia', 'nostra',
+		'inceptos', 'himenaeos', 'phasellus', 'nibh', 'pulvinar', 'vitae',
+		'urna', 'iaculis', 'lobortis', 'nisi', 'viverra', 'arcu',
+		'morbi', 'pellentesque', 'metus', 'commodo', 'ut', 'facilisis',
+		'felis', 'tristique', 'ullamcorper', 'placerat', 'aenean', 'convallis',
+		'sollicitudin', 'integer', 'rutrum', 'duis', 'est', 'etiam',
+		'bibendum', 'donec', 'pharetra', 'vulputate', 'maecenas', 'mi',
+		'fermentum', 'consequat', 'suscipit', 'aliquam', 'habitant', 'senectus',
+		'netus', 'fames', 'quisque', 'euismod', 'curabitur', 'lectus',
+		'elementum', 'tempor', 'risus', 'cras'
+];
+var text = '';
+var btn = document.querySelector('.create');
+var msg = document.querySelector('.output');
+
+
+
+
+btn.addEventListener('click', function () {
+    var amountWords = Number(document.querySelector('#amount').value);
+    for (var i = 0; i <= amountWords; i++) {
+        text += vocab[Math.floor(Math.random() * vocab.length)] + ' ';
+    }
+    msg.textContent = text;
+})
